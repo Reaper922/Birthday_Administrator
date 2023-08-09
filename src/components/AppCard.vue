1545af9a77bdbf0edbe9f96f5e41e1665a281546
Dennis Ammen <dennis.ammen@outlook.com>
2023-08-09 11:59:04 +0200

    Outsourced functions to dateservice

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue';
+import { daysUntilBirthday, formatBirthdate } from '@/services/DateService';
 
 const props = defineProps({
   user: {
@@ -11,36 +11,15 @@ const props = defineProps({
     required: true
   }
 });
-
-const daysUntilBirthday = computed(() => {
-  const currentYear = props.currentDate.getFullYear();
-  const currentUserBirthday = new Date(props.user.birthdate).setFullYear(currentYear);
-  const nextUserBirthday = new Date(props.user.birthdate).setFullYear(currentYear + 1);
-  const diffInMillSec = currentUserBirthday - props.currentDate;
-  const diffInDays = Math.floor(diffInMillSec / (1000 * 60 * 60 * 24));
-
-  console.log(nextUserBirthday);
-
-  if (diffInDays < 0) {
-    return Math.floor((nextUserBirthday - props.currentDate) / (1000 * 60 * 60 * 24));
-  }
-
-  return diffInDays;
-});
-
-const formatedBirthdate = computed(() => {
-  const dateArr = props.user.birthdate.split('-');
-
-  return `${dateArr[2]}.${dateArr[1]}.${dateArr[0]}`;
-});
 </script>
 
 <template>
   <div class="card">
     <h2>{{ user.name }}</h2>
-    <span>Geburtstag: {{ formatedBirthdate }}</span
-    ><br />
-    <span>Noch {{ daysUntilBirthday }} Tage bis zum Geburtstag</span>
+    <p>
+      Geburtstag: {{ formatBirthdate(user.birthdate) }}<br />
+      Noch {{ daysUntilBirthday(props.currentDate, props.user.birthdate) }} Tage bis zum Geburtstag
+    </p>
   </div>
 </template>
 
