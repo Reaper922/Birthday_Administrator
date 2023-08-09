6844798eead546f8c2d8fc05f5a14b117fca3d08
Dennis Ammen <dennis.ammen@outlook.com>
2023-08-09 11:09:08 +0200

    Added basic layout to app component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,110 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import AppSidebar from '@/components/AppSidebar.vue';
+import AppCard from '@/components/AppCard.vue';
+
+const isOpen = ref(false);
+
+const currentDate = new Date();
+const testUser = [
+  {
+    id: 1,
+    name: 'Hans Müller',
+    birthdate: '1992-9-15'
+  },
+  {
+    id: 2,
+    name: 'Julius Meister',
+    birthdate: '1989-04-23'
+  },
+  {
+    id: 3,
+    name: 'Sabine Janssen',
+    birthdate: '1995-06-04'
+  },
+  {
+    id: 4,
+    name: 'Christiane Mittermayer',
+    birthdate: '1994-05-28'
+  },
+  {
+    id: 5,
+    name: 'Karoline Binsen',
+    birthdate: '1984-11-19'
+  }
+];
+
+testUser.sort((userA, userB) => {
+  const currentYear = currentDate.getFullYear();
+  const currentBirthdayUserA = new Date(userA.birthdate).setFullYear(currentYear);
+  const currentBirthdayUserB = new Date(userB.birthdate).setFullYear(currentYear);
+  const diffInDays = currentBirthdayUserA - currentBirthdayUserB;
+
+  if (diffInDays < 0) {
+    const nextBirthdayUserB = new Date(userB.birthdate).setFullYear(currentYear + 1);
+
+    return currentBirthdayUserA - nextBirthdayUserB;
+  } else {
+    return diffInDays;
+  }
+});
+</script>
 
 <template>
-  <h1>Hello World</h1>
+  <AppSidebar :isOpen="isOpen"></AppSidebar>
+  <div class="wrapper" :class="{ 'sidebar-open': isOpen }">
+    <header>
+      <img src="/assets/icons/menu.svg" alt="Menu" class="menu-icon" @click="isOpen = !isOpen" />
+      <h1><strong>B-Shampoo</strong></h1>
+    </header>
+    <main>
+      <h2>Anstehende Geburtstage:</h2>
+      <div class="card-container">
+        <AppCard
+          v-for="user in testUser"
+          :key="user.id"
+          :user="user"
+          :currentDate="currentDate"
+        ></AppCard>
+      </div>
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  flex-grow: 1;
+  margin-left: 0;
+  transition: margin-left var(--transition-time);
+}
+
+.sidebar-open {
+  margin-left: var(--sidebar-width);
+}
+
+header {
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  padding: 8px 24px;
+  background: lightblue;
+  box-shadow: var(--box-shadow-md);
+  z-index: 2;
+}
+
+.menu-icon {
+  height: 32px;
+  cursor: pointer;
+}
+
+main {
+  padding: 32px;
+}
+
+.card-container {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+</style>
